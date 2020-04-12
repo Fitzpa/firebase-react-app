0f838506351d8c909ef39b377efee3b8a4d774bf
unknown <fitz4492@gmail.com>
2020-04-12 18:00:57 -0500

    setting up Firebase Realtime Database in React

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -33,6 +33,9 @@ class SignUpFormBase extends Component {
     this.props.firebase
       .doCreateUserWithEmailAndPassword(email, passwordOne)
       .then(authUser => {
+        // Create a user in your Firebase Realtime Database
+        return this.props.firebase.user(authUser.user.uid).set({username, email});
+      }).then(() => {
         this.setState({ ...INITIAL_STATE });
         this.props.history.push(ROUTES.HOME);
       })
